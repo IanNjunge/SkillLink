cfeccb91fa5594e150345a75d06f5923f8c36e72
Robby <kimanirobinson336@gmail.com>
2025-10-22 15:21:00 +0300

    frontend

--- Client/src/pages/NotFound.jsx
+++ Client/src/pages/NotFound.jsx
@@ -1,1 +1,9 @@
  
+ export default function NotFound() {
+  return (
+    <div className="container" style={{padding: 0}}>
+      <h1 className="title-lg">404 - Not Found</h1>
+      <p className="muted">The page you are looking for does not exist.</p>
+    </div>
+  )
+}
--- Client/src/pages/SearchMentors.jsx
+++ Client/src/pages/SearchMentors.jsx
@@ -21,4 +21,3 @@ export default function Register() {
     </div>
   )
 }
-searchmentor.jsx
