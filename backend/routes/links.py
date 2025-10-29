00d791bd9de6da5d37cf487c8266febf5768db03
robinson732 <kimanirobinson336@gmail.com>
2025-10-30 01:56:55 +0300

    Delete backend directory
    bad

--- backend/routes/links.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-links_bp = Blueprint("links", __name__)
-
-@links_bp.route("/links")
-def get_links():
-    return jsonify({"message": "Links route is working!"})
