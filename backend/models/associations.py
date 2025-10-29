c30e3bd19f67ae1769de24a40fbbab2ea3af2243
robinson732 <kimanirobinson336@gmail.com>
2025-10-29 18:45:43 +0300

    Delete backend directory
    backend is not well polished

--- backend/models/associations.py
+++
@@ -1,8 +0,0 @@
-from ..extensions import db
-
-
-mentor_topic_association = db.Table(
-	"mentor_topics",
-	db.Column("mentor_id", db.Integer, db.ForeignKey("mentor_profiles.id"), primary_key=True),
-	db.Column("topic_id", db.Integer, db.ForeignKey("topics.id"), primary_key=True),
-)
